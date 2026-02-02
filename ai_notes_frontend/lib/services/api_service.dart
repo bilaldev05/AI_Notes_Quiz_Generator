0f5d229c9f6a6602e9befc789ec31d031d076fe7
Bilal Ishaq <bilal.developer05@gmail.com>
2026-02-02 20:54:14 +0500

    Update

--- ai_notes_frontend/lib/services/api_service.dart
+++ ai_notes_frontend/lib/services/api_service.dart
@@ -8,8 +8,7 @@ import '../models/notes_response.dart';
 
 class ApiService {
   static const String baseUrl = "http://localhost:8000"; 
-  // For Android emulator use: http://10.0.2.2:8000
-
+  
   static Future<NotesResponse> uploadAndProcess({
     required String fileName,
     Uint8List? bytes,
