56000ac2e4e01e272569d5f3f570b4298a65c7c7
Bilal Ishaq <bilal.developer05@gmail.com>
2026-02-18 20:28:40 +0500

    Update

--- ai_notes_frontend/lib/screens/result_screen.dart
+++ ai_notes_frontend/lib/screens/result_screen.dart
@@ -27,7 +27,7 @@ class ResultScreen extends StatelessWidget {
             Text(result.summary),
             const Divider(height: 32),
 
-            /// NOTES
+            
             const Text(
               "📝 Study Notes",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
